c3290df8dedb686fc409b88b98a75aa7bf5d65ad
SergeyKosarchuk <sergeykosarchuk@gmail.com>
2019-11-04 12:43:17 +0300

    Move BrowserRouter to index.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,24 @@ import React from 'react';
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuildier/BurderBuilder";
 import Checkout from './containers/Checkout/Checkout';
-import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-            <Route path='/add-burger'>
-              <BurgerBuilder />
-            </Route>
-            <Route path='/checkout'>
-              <Checkout />
-            </Route>
-            <Route path='*'>
-              <Redirect to='/add-burger'/>
-            </Route>
-          </Switch>
-        </Layout>
-      </BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route path='/add-burger'>
+            <BurgerBuilder />
+          </Route>
+          <Route path='/checkout'>
+            <Checkout />
+          </Route>
+          <Route path='*'>
+            <Redirect to='/add-burger'/>
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
