741226517adcbfd04d3461e5674bd1417f11d67d
SergeyKosarchuk <sergeykosarchuk@gmail.com>
2019-11-04 13:09:20 +0300

    Use default route for burger builder

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React from 'react';
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuildier/BurderBuilder";
 import Checkout from './containers/Checkout/Checkout';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Layout>
         <Switch>
-          <Route path='/add-burger'>
-            <BurgerBuilder />
-          </Route>
           <Route path='/checkout'>
             <Checkout />
           </Route>
-          <Route path='*'>
-            <Redirect to='/add-burger'/>
+          <Route path='/'>
+            <BurgerBuilder />
           </Route>
         </Switch>
       </Layout>
