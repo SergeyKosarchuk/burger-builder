f4f767980a48937c6d25c68344083b304671362a
SergeyKosarchuk <sergeykosarchuk@gmail.com>
2019-12-03 11:19:19 +0300

    Add styling

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,17 @@ import { Input } from '../../components/UI/Input/Input';
 import Button, { ACCEPT_TYPE } from '../../components/UI/Button/Button';
 
 const StyledAuth = styled.div`
+    margin: 20px auto;
+    width: 80%;
+    text-align: center;
+    box-shadow: 0 2px 3px #ccc;
+    border: 1px solid #eee;
+    padding: 10px;
+    box-sizing: border-box;
+
+    @media ( min-width: 600px ) {
+        width: 500px;
+    }
 `;
 
 
@@ -41,9 +52,9 @@ export default class Auth extends React.Component {
         return (
             <StyledAuth>
                 <form>
-                    <Input value={email} fieldName='email' onChange={this.handleInput}/>
-                    <Input value={password} fieldName='password' onChange={this.handleInput}/>
-                    <Button type={ACCEPT_TYPE} clicked={this.authHandler} disabled={disabled}>SAVE</Button>
+                    <Input value={email} fieldName='email' onChange={this.handleInput} placeholder='Email'/>
+                    <Input value={password} fieldName='password' onChange={this.handleInput} placeholder='Password'/>
+                    <Button type={ACCEPT_TYPE} clicked={this.authHandler} disabled={disabled}>SUBMIT</Button>
                 </form>
             </StyledAuth>
         );
