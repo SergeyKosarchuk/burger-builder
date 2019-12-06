1fae658fabc2026863d0a46e9f92c96a14282584
SergeyKosarchuk <sergeykosarchuk@gmail.com>
2019-12-06 16:35:53 +0300

    Add second test for NavigationItems

--- src/components/UI/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItems.test.js
@@ -5,8 +5,18 @@ import NavigationItems from './NavigationItems';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 describe('<NavigationItems ./>', () => {
+    let wrapper = null;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />)
+    });
+
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    })
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     })
 })
